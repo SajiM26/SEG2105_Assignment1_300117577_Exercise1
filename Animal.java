dd2a823fe059ef01d3cb6d405aeb47dfb9477d86
SajiM26 <sajida.mohamed@outlook.com>
2025-05-19 06:16:17 -0400

    Completed Animal Super

--- Animal.java
+++ Animal.java
@@ -1,3 +1,29 @@
+import java.lang.reflect.Constructor;
+
 public abstract class Animal implements SoundsMaker {
-    
+    private static int numberOfAnimals = 0;
+    private String name;
+
+    //Contructors
+
+    public Animal(){
+        System.out.println("Animal Constructor called");
+        numberOfAnimals++;
+        name = "Unknown Animal";
+    }
+
+    public Animal(String named){
+        System.out.println("Animal Constructor with name called");
+        numberOfAnimals++;
+        name = named;
+    }
+
+    //Methods
+
+    public abstract String getAnimalType();
+
+    public int getNumberOfAnimals(){
+        return numberOfAnimals;
+    }
+
 }
