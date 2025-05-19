ca30fe6ee7afc69a52e61458cd63cce486a82a73
SajiM26 <sajida.mohamed@outlook.com>
2025-05-19 06:46:32 -0400

    Completed parrot with Dog aas base

---
+++ Parrot.java
@@ -0,0 +1,20 @@
+public class Parrot extends Bird {
+    //Constructors
+    public Parrot(){
+        super();
+        System.out.println("Parrot Constructor called");
+    }
+
+    public Parrot(String name){
+        super(name);
+        System.out.println("Parrot Constructor with name called");
+    }
+
+    public void makeSound(){
+        System.out.println("Mimic or chirp");
+    }
+
+    public String getAnimalType(){
+        return "Parrot";
+    }
+}
