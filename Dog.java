192f9e393a3d5db535092c2fef9a8bbd3f7ba007
SajiM26 <sajida.mohamed@outlook.com>
2025-05-19 06:47:30 -0400

    Completed Dog class

--- Dog.java
+++ Dog.java
@@ -1,3 +1,22 @@
-public class Dog {
+public class Dog extends Mammal{
     
+    //Constructors
+    public Dog(){
+        super();
+        System.out.println("Dog Constructor called");
+    }
+
+    public Dog(String name){
+        super(name);
+        System.out.println("Dog Constructor with name called");
+    }
+
+    //Methods
+    public void makeSound(){
+        System.out.println("Bark Bark, woof woof");
+    }
+
+     public String getAnimalType(){
+        return "Dog";
+    }
 }
